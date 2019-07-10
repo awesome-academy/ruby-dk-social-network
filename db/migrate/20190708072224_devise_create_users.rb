8dd27f6fe75b458aef417d54533a8f5c61335aab
smiled <nguyenduc.blue@gmail.com>
2019-07-10 14:57:46 +0700

    Show user

--- db/migrate/20190708072224_devise_create_users.rb
+++ db/migrate/20190708072224_devise_create_users.rb
@@ -1,9 +1,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :user_name
-      t.string :website
+      t.string :name, default: "Updating"
+      t.string :user_name, default: "Updating"
+      t.string :website, default: "Updating"
       t.string :avatar
       t.string :email, null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
